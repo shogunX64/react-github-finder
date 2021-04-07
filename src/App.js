751ca663e95ef0a62b1484c51ff4dd9f95b534d4
Andrei B <ruby.project15@gmail.com>
2021-04-07 12:32:29 +0300

    added alert for submit with empty search field

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // Search Github users
@@ -24,16 +26,24 @@ class App extends Component {
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
+  
+  //set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }})
+    setTimeout(() => this.setState({alert: null }), 5000)
+  }
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert = {this.setAlert}
           />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
