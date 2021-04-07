76812ed07baea49fbd8f4aad7264187d1145aa37
Andrei B <ruby.project15@gmail.com>
2021-04-07 11:29:44 +0300

    added search functionality - not working correctly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 import './App.css';
 
@@ -9,16 +10,19 @@ class App extends Component {
     users: [],
     loading: false
   }
-  async componentDidMount(){
-    this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false})
+
+  //serach github users
+  searchUsers = async text => {
+    this.setState({ loading: true }) 
+    const res = await axios.get(`https://api.github.com/serach/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false})
   }
   render(){
     return (
       <div className="App">
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
