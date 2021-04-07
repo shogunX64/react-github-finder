dc7ca6d5b1cd2d771fff351eac7f46d95b466973
Andrei B <ruby.project15@gmail.com>
2021-04-07 11:58:24 +0300

    fixed search function and added clear button

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
-import axios from 'axios'
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
     loading: false
-  }
+  };
 
-  //serach github users
+  // Search Github users
   searchUsers = async text => {
-    this.setState({ loading: true }) 
-    const res = await axios.get(`https://api.github.com/serach/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
-    this.setState({ users: res.data.items, loading: false})
-  }
-  render(){
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers}/>
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
   }
 }
-
 export default App;
